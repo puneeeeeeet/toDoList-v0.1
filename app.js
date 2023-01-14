6d91f72897277255c0c80a9bf1e0fa77cfa3f307
puneeeeeeet <puneetchhabra1607@gmail.com>
2023-01-14 13:44:36 +0530

    completed

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+
+var items = ["Wake-up"];
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day:"numeric",
+        month:"long",
+    };
+var day = today.toLocaleDateString("en-US",options);
+    res.render("list",{typeOfDay: day,newListItems: items});
+});
+
+    app.post("/",function(req,res){
+        var item = req.body.itemName;
+        items.push(item); 
+        res.redirect("/");  
+
+    });
+    // app.get("/work")
+
+app.listen(3000,function(){
+    console.log("Working on port 3000.")
+});
